6083eb3b5a0ca9bab2f088a1dec0c88ec310c210
rightup-group <76165245+rightup-group@users.noreply.github.com>
2020-12-17 22:50:56 +0900

    Update header.js

--- header.js
+++ header.js
@@ -1,8 +1,8 @@
-document.write('<a href="index.html"><img src="http://drive.google.com/uc?export=view&id={1ywF7T1YqW6kte0MeULrn-7Kvt8PoIQ3T}" id="logo-header" alt="ロゴ画像"></a>');
+document.write('<a href="index.html"><img src="https://cdn-ak.f.st-hatena.com/images/fotolife/d/douga-no-sozaiko/20201217/20201217224415.png" id="logo-header" alt="ロゴ画像"></a>');
 document.write('<ul>');
 document.write('<li class="link-a"><a href="index.html">Home</a></li>');
 document.write('<li class="link-a"><a href="#medias">Medias</a></li>');
 document.write('<li class="link-a"><a href="about.html">About</a></li>');
 document.write('<li class="link-a"><a href="contact.html">Contact</a></li>');
-document.write('<li><a href="https://twitter.com/"><img src="http://drive.google.com/uc?export=view&id={1RTZ4uQ_YtaSz_xGALagx0riod2WzqQmz}" id="twitter-header" alt="Twitterアイコン" class="link-a"></a></li>');
+document.write('<li><a href="https://cdn-ak.f.st-hatena.com/images/fotolife/d/douga-no-sozaiko/20201217/20201217224440.png" id="twitter-header" alt="Twitterアイコン" class="link-a"></a></li>');
 document.write('</ul>');
