146a7e289f17ca041e1e1911ee5c53a616a95490
rightup-group <76165245+rightup-group@users.noreply.github.com>
2020-12-17 22:49:46 +0900

    Update head.js

--- head.js
+++ head.js
@@ -1,4 +1,4 @@
 document.write('<meta charset="UTF-8">');
 document.write('<meta http-equiv="X-UA-Compatible" content="IE=edge" />');
-document.write('<link rel="shortcut icon" href="http://drive.google.com/uc?export=view&id={1tyc3Ik0D1cgg2YcwB1BaIydcuO9tK3oj}">');
+document.write('<link rel="shortcut icon" href="https://cdn-ak.f.st-hatena.com/images/fotolife/d/douga-no-sozaiko/20201217/20201217224411.png">');
 document.write('<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.4/css/all.css">');
