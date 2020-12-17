20014da273404603bc3a66fdf3495e626333daee
rightup-group <76165245+rightup-group@users.noreply.github.com>
2020-12-17 22:26:40 +0900

    Update header.js

--- header.js
+++ header.js
@@ -1,8 +1,8 @@
-document.write('<a href="index.html"><img src="images/logo-mark.png" id="logo-header" alt="ロゴ画像"></a>');
+document.write('<a href="index.html"><img src="http://drive.google.com/uc?export=view&id={1ywF7T1YqW6kte0MeULrn-7Kvt8PoIQ3T}" id="logo-header" alt="ロゴ画像"></a>');
 document.write('<ul>');
 document.write('<li class="link-a"><a href="index.html">Home</a></li>');
 document.write('<li class="link-a"><a href="#medias">Medias</a></li>');
 document.write('<li class="link-a"><a href="about.html">About</a></li>');
 document.write('<li class="link-a"><a href="contact.html">Contact</a></li>');
-document.write('<li><a href="https://twitter.com/"><img src="images/Twitter_Social_Icon_Circle_Color.png" id="twitter-header" alt="Twitterアイコン" class="link-a"></a></li>');
+document.write('<li><a href="https://twitter.com/"><img src="http://drive.google.com/uc?export=view&id={1RTZ4uQ_YtaSz_xGALagx0riod2WzqQmz}" id="twitter-header" alt="Twitterアイコン" class="link-a"></a></li>');
 document.write('</ul>');
