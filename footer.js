72d24ed05bcffce4464a38a5136f6c439652e62b
rightup-group <76165245+rightup-group@users.noreply.github.com>
2020-12-17 22:48:49 +0900

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,8 +1,8 @@
 document.write('<div id="sites">');
-document.write('<a href="index.html"><img src="http://drive.google.com/uc?export=view&id={1u90BNFuPLDlw23IcluDZAYpiUzvSMi1g}" alt="「RightUP」ロゴ" id="footer-main-logo"></a>');
+document.write('<a href="index.html"><img src="https://cdn-ak.f.st-hatena.com/images/fotolife/d/douga-no-sozaiko/20201217/20201217224419.png" alt="「RightUP」ロゴ" id="footer-main-logo"></a>');
 document.write('<div id="footer-sub-logos">');
 document.write('<ul>');
-document.write('<li class="link-a"><a href="https://douga-no-sozaiko.hateblo.jp/"><img src="http://drive.google.com/uc?export=view&id={1aenDZPQC6Mhl79aD9cBVpPpbLhyL1BVx}" alt="「動画の素材庫」ロゴ"></a></li> ');
+document.write('<li class="link-a"><a href="https://douga-no-sozaiko.hateblo.jp/"><img src="https://cdn-ak.f.st-hatena.com/images/fotolife/d/douga-no-sozaiko/20201217/20201217224505.png" alt="「動画の素材庫」ロゴ"></a></li> ');
 document.write('</ul>');
 document.write('</div>');
 document.write('</div>');
