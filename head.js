69f06546b958436d920720dd82dc33ed6d9a0ccf
rightup-group <76165245+rightup-group@users.noreply.github.com>
2020-12-17 22:24:46 +0900

    Update head.js

--- head.js
+++ head.js
@@ -1,4 +1,4 @@
 document.write('<meta charset="UTF-8">');
 document.write('<meta http-equiv="X-UA-Compatible" content="IE=edge" />');
-document.write('<link rel="shortcut icon" href="images/aikon.png">');
+document.write('<link rel="shortcut icon" href="http://drive.google.com/uc?export=view&id={1tyc3Ik0D1cgg2YcwB1BaIydcuO9tK3oj}">');
 document.write('<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.4/css/all.css">');
