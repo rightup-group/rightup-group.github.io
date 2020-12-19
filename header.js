4ccb6d8dabc8a8cc4929fc930170154f85559955
rightup-group <76165245+rightup-group@users.noreply.github.com>
2020-12-19 19:25:21 +0900

    Update header.js

--- header.js
+++ header.js
@@ -1,7 +1,7 @@
 document.write('<a href="index.html"><img src="https://cdn-ak.f.st-hatena.com/images/fotolife/d/douga-no-sozaiko/20201217/20201217224415.png" id="logo-header" alt="ロゴ画像"></a>');
 document.write('<ul>');
 document.write('<li class="link-a"><a href="index.html">Home</a></li>');
-document.write('<li class="link-a"><a href="#media-list">Medias</a></li>');
+document.write('<li class="link-a"><a href="index.html/#medias">Medias</a></li>');
 document.write('<li class="link-a"><a href="about.html">About</a></li>');
 document.write('<li class="link-a"><a href="contact.html">Contact</a></li>');
 document.write('<li><a href="https://twitter.com/"><img src="https://cdn-ak.f.st-hatena.com/images/fotolife/d/douga-no-sozaiko/20201217/20201217224440.png" id="twitter-header" alt="Twitterアイコン" class="link-a"></a></li>');
