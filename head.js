744cbe458ad52db42f89924fd85ca8f255f1da75
rightup-group <76165245+rightup-group@users.noreply.github.com>
2021-01-02 17:32:02 +0900

    Update head.js

--- head.js
+++ head.js
@@ -3,3 +3,14 @@ document.write('<meta http-equiv="X-UA-Compatible" content="IE=edge" />');
 document.write('<link rel="shortcut icon" href="https://cdn-ak.f.st-hatena.com/images/fotolife/d/douga-no-sozaiko/20201217/20201217224411.png">');
 document.write('<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.4/css/all.css">');
 document.write('<head prefix="og: http://ogp.me/ns#"></head> <!-- OGP宣言 -->');
+document.write('
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-884WNSECXC"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-884WNSECXC');
+</script>
+');
